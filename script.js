1de50aa40a7ab3a69dded34014907b18da1ca87d
Isabela Cirino <dbela.cirino@outlook.com>
2023-01-24 13:54:15 -0300

    funções adicionadas para atualização do carrinho

--- script.js
+++ script.js
@@ -5,13 +5,45 @@ let valorSubtotal = document.getElementById("valor-subtotal");
 const addProduto = document.getElementById("btn-adicionar-produto-01");
 const removeProduto = document.getElementById("btn-subtrair-produto-01");
 const quantidadeProduto = document.getElementById("quantidade-produto-01");
-//const title = document.getElementsByTagName("h2");
+const title = document.getElementsByTagName("h2");
 
 let subtotalInfo = {
   valor: 11.66,
 };
 
+
+
 // EVENTOS A SEREM EXECUTADOS COM INTERAÇÃO DO USUÁRIO CRIANDO FUNÇÕES
 addProduto.addEventListener("click", adicionarProduto)
 removeProduto.addEventListener("click", subtrairProduto)
 
+
+
+//CRIANDO FUNÇÕES PARA ATUALIZAR ITENS DO CARRINHO
+
+function updateSubtotalValue (quantidadeProduto){
+  valorSubtotal.innerText = (subtotalInfo.valor * quantidadeProduto).toFixed(2);
+}
+
+function updateSubtotalText(){
+  quantidadeSubtotal.innerText = quantidadeProduto.value ==1 ? `${quantidadeProduto.value}item` : `${quantidadeProduto.value}itens`;
+  updateSubtotalValue (quantidadeProduto.value)
+}
+
+
+function adicionarProduto(){
+  if (quantidadeProduto.value < 10) {
+    quantidadeProduto.value = Number (quantidadeProduto.value) + 1 
+  //} else {
+ //  title[1].innerText = "Máximo 10 produtos no carrinho"
+  }
+  updateSubtotalText()
+}
+
+
+function subtrairProduto(){
+  if (quantidadeProduto.value > 1){
+    quantidadeProduto.value = Number (quantidadeProduto.value) - 1
+  }
+  updateSubtotalText()
+ }
